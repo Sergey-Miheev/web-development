fc43343e5a9ff92c6002a437667972b424a8cdb4
Sergey Miheev <seramiheev302@gmail.com>
2021-03-07 20:31:29 +0300

    fixed a logical error in the code

--- lw2/23/23.pas
+++ lw2/23/23.pas
@@ -3,18 +3,26 @@ USES
   DOS;
 VAR
   QuerStr, NameOfUser: STRING;
-  Position: LongInt;
+  AmperPosition, Position: LongInt;
 BEGIN
   QuerStr := GetEnv('QUERY_STRING');
   WRITELN('Content-Type: text/plain');
   WRITELN;
   Position := Pos('name', QuerStr);
+  AmperPosition := Pos('&', QuerStr);
   IF (Position <> 0)
   THEN
-    BEGIN
-      NameOfUser := Copy(QuerStr, Position + 5, Length(QuerStr) - 5);
-      WRITELN('Hello dear, ', NameOfUser, '!')
-    END
+    IF (AmperPosition <> 0)
+    THEN
+      BEGIN 
+        NameOfUser := Copy(QuerStr, Position + 5, AmperPosition);
+        WRITELN('Hello dear, ', NameOfUser, '!')
+      END  
+    ELSE
+      BEGIN
+        NameOfUser := Copy(QuerStr, Position + 5, Length(QuerStr) - 5);
+        WRITELN('Hello dear, ', NameOfUser, '!')
+      END
   ELSE
     WRITELN('Hello Anonymous!')
 END.
