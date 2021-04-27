ec6920d39c92f5a974b7be7624d7c2cf2a13f6f4
Sergey Miheev <seramiheev302@gmail.com>
2021-04-27 20:55:07 +0300

    Исправил ошибки и оптимизировал программу

--- lw8/js/script1.js
+++ lw8/js/script1.js
@@ -1,43 +1,40 @@
 function Identification(number) {
-  let isPrime
+  let isPrime;
   for (let i = 2; i <= number; i++) {
-    isPrime = true
+    isPrime = true;
     for (let j = 2; j < i; j++) {
       if (i % j === 0) {
-        isPrime = false
-        break
+        isPrime = false;
+        break;
       }  
     }
-  }  
-if (isPrime) {
-  return console.log('Число ', number, 'простое.')
+  } 
+  result(isPrime, number); 
 }  
-else {
-  return console.log('Число', number, ' не простое.')
-}     
-}
+function result(isPrime, number) {
+  if (isPrime) {
+    console.log('Число ', number, 'простое.');
+  } else {
+    console.log('Число', number, ' не простое.');
+  }    
+}  
 function isPrimeNumber(value) {
   if (typeof (value) === 'number') {
-    Identification(value)
-  } 
-  else if (value instanceof Array) {
-    let check = true
-    if (value.length !== 0) {
+    Identification(value);
+  } else if (value instanceof Array) {
+    if (value.length) {
       for (let i = 0; i < value.length; i++) {
         if (typeof (value[i]) !== 'number') {
-          check = false
-          return console.log('Введены некорректные данные(введите числа)')
+          return console.log('Введены некорректные данные(введите числа)');  
+        } else {
+          Identification(value[i]);
         }        
       }
-      for (let i = 0; i < value.length; i++) {
-        Identification(value[i])
-      }
-    } 
-    else {
+    } else {
       return console.log('Ваш массив пустой!')
     }
   } 
   else {
-    return console.log('Введите массив или число!')
+  return console.log('Введите массив или число!')
   }
 }
